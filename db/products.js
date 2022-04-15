64a4454ae233953b3afcfd7d1691448dcf6e2214
AgustinGrieben <agrieben@udesa.edu.ar>
2022-04-15 08:43:08 -0300

    detalle de productos terminada, con comentarios y perfiles

--- db/products.js
+++ db/products.js
@@ -13,16 +13,22 @@ let products = [
             img:"https://conceptodefinicion.de/wp-content/uploads/2015/03/hombre.jpg"
         },
         {
-            name : "Enrique",
-            lastName: "Guzman",
-            text:"Altas llantas!",
-            img:"https://conceptodefinicion.de/wp-content/uploads/2015/03/hombre.jpg"
+            name : "Marcelo",
+            lastName: "Gallardo",
+            text:"Muy buena calidad, son originales",
+            img:"https://cloudfront-us-east-1.images.arcpublishing.com/infobae/Q32AWEIQ7NEKREKNAZG3MZWYVE.jpg"
         },
         {
-            name : "Enrique",
-            lastName: "Guzman",
-            text:"Altas llantas!",
-            img:"https://conceptodefinicion.de/wp-content/uploads/2015/03/hombre.jpg"
+            name : "Ester",
+            lastName: "Esposito",
+            text:"comodas y perfectas para lucirlas en un nightclub",
+            img:"https://www.elindependiente.com/wp-content/uploads/2021/10/ester-exposito-premios-platino-scaled.jpg"
+        },
+        {
+            name : "Trueno",
+            lastName: "",
+            text:"La suela me vino rota, espero que me la cambien gratis",
+            img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToFLG5RHFwAj8kNSxp22HqJVo3K9PCg4fYwA&usqp=CAU"
         }
       ]
 },
