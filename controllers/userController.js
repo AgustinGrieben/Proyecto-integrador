bdfcfda36f8e4d38a01c9323512f8b85b862769f
AgustinGrieben <agrieben@udesa.edu.ar>
2022-04-15 18:44:04 -0300

    pagina login sin formulario

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,14 @@
 let user = require("../db/user")
-
+let products = require("../db/products")
 let controlador = {
     register: (req,res)=>{
         res.render("register")
     },
     profile: (req,res)=>{
-        res.render("profile" , {user: user})
+        res.render("profile" , {user: user,productos:products})
     },
+    login: (req,res)=>{
+        res.render("login")
+    }
 }
 module.exports = controlador
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,5 @@ let userController = require("../controllers/userController")
 /* GET users listing. */
 router.get('/register', userController.register);
 router.get('/profile', userController.profile);
-
+router.get('/login', userController.login);
 module.exports = router;
