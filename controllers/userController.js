ba22c9c1ba806999f8eb67cad7f335d2d7811188
luciadenham <ldenham@udesa.edu.ar>
2022-06-23 20:25:57 -0300

    arreglos detalles

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,97 +1,107 @@
 let user = require("../db/user")
 let products = require("../db/products")
-let db = require ("../database/models")
+let db = require("../database/models")
 let users = db.User
 let bcrypt = require("bcryptjs")
 let controlador = {
-    register: (req,res)=>{
-        res.render("register")
-    },
-    profile: (req,res)=>{
+  register: (req, res) => {
+    res.render("register")
+  },
+  profile: (req, res) => {
 
-      users.findByPk(req.params.id, {
-        include:{all: true, nested:true}
-        }).then(usuario => {
-          req.session.user=usuario
-        res.render("profile" , {usuario})
+    users.findByPk(req.params.id, {
+      include: { all: true, nested: true }
+    }).then(usuario => {
+      req.session.user = usuario
+      res.render("profile", { usuario })
+    })
+
+  },
+  login: (req, res) => {
+    res.render("login")
+
+  },
+  profileEdit: (req, res) => {
+    if (req.session.user && req.session.user.id == req.params.id) {
+      users.findByPk(req.params.id).then(usuario => {
+        res.render("profile-edit", { usuario })
       })
-      
-    },
-    login: (req,res)=>{
-        res.render("login")
+    } else {
+      res.redirect("/")
+    }
+  },
 
-    },
-    profileEdit:  (req,res)=>{
-      if(req.session.user && req.session.user.id == req.params.id) {
-        users.findByPk(req.params.id).then(usuario => {
-          res.render("profile-edit" , {usuario})
-        })
-      } else {
-        res.redirect("/")
-      }
-    },
-
-   edit:  (req,res)=>{
+  edit: (req, res) => {
     let usuario = {
       email: req.body.email,
       username: req.body.usuario,
       date: req.body.fecha,
       dni: req.body.dni,
     }
-    if(req.body.foto) usuario.image = req.body.foto
-    if(req.body.password)usuario.password = bcrypt.hashSync(req.body.password,10)
-    users.update(usuario,{
-      where:{
-        id:req.params.id
+    if (req.body.foto) usuario.image = req.body.foto
+    if (req.body.password) usuario.password = bcrypt.hashSync(req.body.password, 10)
+    users.update(usuario, {
+      where: {
+        id: req.params.id
       }
     })
-    .then(function(){
-      return res.redirect("/users/profile/"+req.params.id)
-    })
-   },
+      .then(function () {
+        return res.redirect("/users/profile/" + req.params.id)
+      })
+  },
 
-    storeUser: (req,res) =>{
-      let password = bcrypt.hashSync(req.body.password,10)
-      let usuario = {
-        email: req.body.email,
-        username: req.body.usuario,
-        password: password,
-        date: req.body.fecha,
-        dni: req.body.dni,
-        image: req.body.foto 
-      }
+  storeUser: (req, res) => {
+    let password = bcrypt.hashSync(req.body.password, 10)
+    let usuario = {
+      email: req.body.email,
+      username: req.body.usuario,
+      password: password,
+      date: req.body.fecha,
+      dni: req.body.dni,
+      image: req.body.foto
+    }
 
-      users.create(usuario)
-      .then(function(){
+    users.create(usuario)
+      .then(function () {
         return res.redirect("/")
       })
 
-    },
+  },
 
-sesion: (req,res)=>{
-  users.findOne({
-    where:{
-      email: req.body.email
-    }
-  }) .then(user=>{
-    if(user){
-      if(bcrypt.compareSync(req.body.password, user.password)){
-        req.session.user= user
-        if(req.body.remember) {
-          res.cookie("userId", user.id,{
-            maxAge: 1000*60*5
-          })
-        }
-        res.redirect("/")
+  sesion: (req, res) => {
+    let errors = {}
+    users.findOne({
+      where: {
+        email: req.body.email
       }
-    }
-  })
-},
+    }).then(user => {
+      if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          req.session.user = user
+          if (req.body.remember) {
+            res.cookie("userId", user.id, {
+              maxAge: 1000 * 60 * 5
+            })
+          }
+          res.redirect("/")
+        } else {
+          errors.message = "contraseña incorrecta"
+          res.locals.errors = errors
+          return res.render("login")
+        }
+      }
+      else {
+        errors.message = "email es incorrecto"
+        res.locals.errors = errors
+        return res.render("login")
+      }
+    })
+  },
 
-logout:(req,res)=>{
-  req.session.user= null
-  res.clearCookie("userId")
-  res.redirect("/")
-},
+  logout: (req, res) => {
+    req.session.user = null
+    res.clearCookie("userId")
+    res.redirect("/")
+  },
 }
 module.exports = controlador
